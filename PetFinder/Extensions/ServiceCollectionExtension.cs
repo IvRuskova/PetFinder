cc3a71f148b84bf4c7aae656e0f15d1388ce8813
Iva <IvRuskova@gmail.com>
2025-07-20 23:56:20 +0300

    ServiceCollectionExtension added to simplify Program.cs

---
+++ PetFinder/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PetFinder.Data;
+
+namespace PetFinder.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddPetFinderServices(this IServiceCollection services)
+        {
+            return services;
+        }
+        public static IServiceCollection AddPetFinderDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            return services;
+        }
+        public static IServiceCollection AddPetFinderIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            return services;
+        }
+    }
+}
