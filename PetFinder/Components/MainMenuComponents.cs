ec367202c8d2b71e483691b0275f940fb1f6d278
Iva <IvRuskova@gmail.com>
2025-07-22 07:56:10 +0300

    Add MainMenu componets

---
+++ PetFinder/Components/MainMenuComponents.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetFinder.Components
+{
+    public class MainMenuComponents : ViewComponent
+
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // You can pass any data to the view if needed
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
