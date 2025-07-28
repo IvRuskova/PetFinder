451070c541adbaf3c0929754182ba5d3e545a2bf
Iva <IvRuskova@gmail.com>
2025-07-28 14:13:32 +0300

    Repository added

---
+++ PetFinder.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PetFinder.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext _context;
+
+        public Repository(PetFinderDbContext context)
+        {
+            _context = context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return _context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+    }
+}
